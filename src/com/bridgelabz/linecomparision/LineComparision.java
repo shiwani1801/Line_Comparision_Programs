b4c2e809cc3cf3ac3b71c786079d3732f223006b
shiwani <shiwanipaunikar1@gmail.com>
2023-01-06 12:20:54 -0800

    eqality  of line program is added

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -3,10 +3,8 @@ package com.bridgelabz.linecomparision;
 import java.util.Scanner;
 
 public class LineComparision {
-
         public static void main(String[] args) {
-            int x1,x2,y1,y2;
-            double Length_Of_Line;
+            int x1,x2,y1,y2,a1,a2,b1,b2;
             Scanner scan=new Scanner(System.in);
             System.out.println("enter x1 point");
             x1=scan.nextInt();
@@ -16,11 +14,30 @@ public class LineComparision {
             y1=scan.nextInt();
             System.out.println("enter y2 point");
             y2=scan.nextInt();
-            //retrun  sq root of no.
-            Length_Of_Line=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-            System.out.println("distance between point is"+"("+x1+","+x2+"),"+"("+y1+","+y2+")="+Length_Of_Line);
+            double line1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 
+            System.out.println(line1);
+            System.out.println("distance between point is"+"("+x1+","+x2+"),"+"("+y1+","+y2+"):"+line1);
+            System.out.println("enter a1 point");
+            a1=scan.nextInt();
+            System.out.println("enter a2 point");
+            a2=scan.nextInt();
+            System.out.println("enter b1 point");
+            b1=scan.nextInt();
+            System.out.println("enter b2 point");
+            b2=scan.nextInt();
+            double line2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+            System.out.println(line2);
+            System.out.println("distance between point is"+"("+a1+","+a2+"),"+"("+b1+","+b2+"):"+line2);
+            Double Length_Of_Line_1=line1;
+            Double Length_Of_Line_2=line2;
+
+            if((Length_Of_Line_1).equals(Length_Of_Line_2)) {
+                System.out.println("Both are Equal");
+            }else{
+                System.out.println("Both are not Equal");
+
+            }
         }
-
-
     }
+
