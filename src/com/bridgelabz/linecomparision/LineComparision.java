efb2f21c7cb0319681cc588c6f33d48c3def2682
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:23:57 -0800

    Added: added equal line program

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -7,13 +7,13 @@ public class LineComparision {
             int x1,x2,y1,y2,a1,a2,b1,b2;
             Scanner scan=new Scanner(System.in);
             System.out.println("enter x1 point");
-            x1=scan.nextInt();
+            x1 = scan.nextInt();
             System.out.println("enter x2 point");
-            x2=scan.nextInt();
+            x2 = scan.nextInt();
             System.out.println("enter y1 point");
-            y1=scan.nextInt();
+            y1 = scan.nextInt();
             System.out.println("enter y2 point");
-            y2=scan.nextInt();
+            y2 = scan.nextInt();
             double line1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 
             System.out.println(line1);
