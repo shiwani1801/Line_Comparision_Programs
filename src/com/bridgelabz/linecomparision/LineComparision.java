594e5ac7ebfa1587c4f8fe18c8cfd306c4b5a65f
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:22:23 -0800

    Added: added compare line program

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -7,13 +7,13 @@ public class LineComparision {
         int x1,x2,y1,y2,a1,a2,b1,b2;
         Scanner scan=new Scanner(System.in);
         System.out.println("enter x1 point");
-        x1=scan.nextInt();
+        x1 = scan.nextInt();
         System.out.println("enter x2 point");
-        x2=scan.nextInt();
+        x2 = scan.nextInt();
         System.out.println("enter y1 point");
-        y1=scan.nextInt();
+        y1 = scan.nextInt();
         System.out.println("enter y2 point");
-        y2=scan.nextInt();
+        y2 = scan.nextInt();
         //return sq root of no
         double line1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 
